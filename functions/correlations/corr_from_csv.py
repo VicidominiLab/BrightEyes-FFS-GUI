9f095a81d838bd700b3a07baf0eaa3416bcdc834
eslenders <elislenders@hotmail.com>
2026-08-27 10:15:21 +0200

    small fixes gui layout

---
+++ functions/correlations/corr_from_csv.py
@@ -0,0 +1,7 @@
+def get_params(det_type='Square 5x5'):
+    return {
+        "mode"       : 'Curve from file',
+        "elements"   : ['det0_averageX'],
+        "listOfG"    : ['det0_averageX'],
+        "shortlabel" : 'single-el-fcs',
+    }
